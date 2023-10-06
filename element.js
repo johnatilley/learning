d2a74567b78058c42bc513d1d41aa52b7a394139
johnatilley <1987301+johnatilley@users.noreply.github.com>
2023-10-06 18:10:08 +0100

    Chapter 6

---
+++ element.js
@@ -0,0 +1,34 @@
+const Element = {
+    data () {
+        return {
+            input: false
+        }
+    },
+    template: `
+        <li>
+            <span v-if="!input"> {{text}} </span>
+            <input v-else type="text" :value="text" @blur="modify($event)" ref="refInput" />
+            <button @click="remove()"> Remove </button>
+            <button @click="input=true"> Modify </button>
+        </li>
+    `,
+    props: [ "text", "index" ],
+    methods: {
+        remove () {
+            this.$emit( "remove", { index: this.index } );
+        },
+        modify ( event ) {
+            var value = event.target.value;
+            this.input = false;
+            this.$emit( "modify", { index: this.index, value: value } );
+        }
+    },
+    emits: [ "remove", "modify" ],
+    updated () {
+        if ( this.$refs.refInput ) {
+            this.$refs.refInput.focus();
+        }
+    }
+}
+
+export default Element;
