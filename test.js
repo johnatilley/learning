ade19802e013383355fb4d2913fe9eb5613e96e2
johnatilley <1987301+johnatilley@users.noreply.github.com>
2023-10-09 18:56:32 +0100

    Node

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
-var mod1 = require( "module1" );
-console.log( "mod1 =", mod1 );
+var http = require( "http" );
 
-var total = mod1( 2, 3 );
-console.log( "mod1(2, 3) = ", total );
+var server = http.createServer( function ( req, res ) {
+    // Display the received request in the console
+    console.log( "Request received:", req.url );
+    // indicate that the response is HTML in utf-8
+    res.setHeader( "Content-type", "text/html; charset==utf-8" );
 
-var total = mod1.mult( 2, 3 );
-console.log( "mod1.mult(2, 3) = ", total );
+    res.write( "<h1>" );
+    res.write( "Hello, world!" );
+    res.write( "</h1>" );
+    res.end();
+} );
+
+server.listen( 3000 );
+
+console.log( "\nThe server was started on port 3000\n" );
+console.log( "You can make a request on:" );
+console.log( "http://localhost:3000" );
